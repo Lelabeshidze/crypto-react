fbe0787af83b63eaad49ce6feed86af13510f1f4
Lelabeshidze <94396022+Lelabeshidze@users.noreply.github.com>
2023-05-24 18:25:31 +0400

    added coin details

--- src/components/Cryptodetails.jsx
+++ src/components/Cryptodetails.jsx
@@ -132,6 +132,46 @@ const Cryptodetails = () => {
             </Col>
           ))}
         </Col>
+        <Col className="other-stats-info">
+          <Col className="coin-value-statistics-heading">
+            <Title level={3} className="coin-details-heading">
+              Other Statistics
+            </Title>
+            <p>An overview showing the stats of all cryptocurrencies</p>
+          </Col>
+          {genericStats.map(({ icon, title, value }) => (
+            <Col className="coin-stats">
+              <Col className="coin-stats-name">
+                <Text>{icon}</Text>
+                <Text>{title}</Text>
+              </Col>
+              <Text className="stats">{value}</Text>
+            </Col>
+          ))}
+        </Col>
+      </Col>
+      <Col className="coin-desc-link">
+        <Row className="coin-desc">
+          <Title level={3} className="coin-details-heading">
+            What is {cryptoDetails.name} <br />
+            {HTMLReactParser(cryptoDetails.description)}
+          </Title>
+        </Row>
+        <Col className="coin-links">
+          <Title level={3} className="coin-details-heading">
+            {cryptoDetails.name} Links
+          </Title>
+          {cryptoDetails.links.map((link) => (
+            <Row className="coin-link" key={link.name}>
+              <Title level={5} className="link-name">
+                {link.type}
+                <a href={link.url} target="_blank" rel="noreferrer">
+                  {link.name}
+                </a>
+              </Title>
+            </Row>
+          ))}
+        </Col>
       </Col>
     </Col>
   );
